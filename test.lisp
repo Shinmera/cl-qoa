8560aa7d53f74b38d330caf2d84d455df94b5456
Shinmera <shinmera@tymoon.eu>
2024-06-01 14:35:09 +0200

    trivial test

--- test.lisp
+++ test.lisp
@@ -7,4 +7,14 @@
 
 (define-test qoa)
 
+(defun generate (count gen)
+  (map-into (make-array count :element-type '(signed-byte 16)) gen))
 
+(defun silence (&optional (length 128))
+  (generate length (constantly 0)))
+
+(define-test trivial-en/decode
+  :parent qoa
+  (finish (qoa:encode (silence) :channels 1))
+  (finish (qoa:decode (qoa:encode (silence) :channels 1)))
+  (is equalp (silence) (qoa:decode (qoa:encode (silence) :channels 1))))
