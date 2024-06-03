3115889b4d379aa33d6559a010887e9f7340cabd
Shinmera <shinmera@tymoon.eu>
2024-06-03 14:56:06 +0200

    misc

--- documentation.lisp
+++ documentation.lisp
@@ -1,3 +1,41 @@
 (in-package #:org.shirakumo.qoa)
 
-(docs:define-docs)
+(docs:define-docs
+  (type file
+    "")
+  
+  (function octet-size
+    "")
+  
+  (function samplerate
+    "")
+  
+  (function channels
+    "")
+  
+  (function read-file
+    "")
+  
+  (function write-file
+    "")
+  
+  (function encode-from-buffer
+    "")
+  
+  (function encode-file
+    "")
+  
+  (function decode-frame
+    "")
+  
+  (function decode-to-buffer
+    "")
+  
+  (function decode-file
+    "")
+  
+  (function convert-wav
+    "")
+  
+  (function channel-layout
+    ""))
