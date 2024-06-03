a17c7c9f1203ba6df95b929e8240f564a01c9f2b
Shinmera <shinmera@tymoon.eu>
2024-06-03 13:59:29 +0200

    More test cases

--- test.lisp
+++ test.lisp
@@ -5,6 +5,8 @@
 
 (in-package #:org.shirakumo.qoa.test)
 
+(defparameter *qoaconv* #p "~/Projects/c/qoa/qoaconv")
+
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defvar *samples-directory*
     #.(merge-pathnames "samples/" (make-pathname :name NIL :type NIL :defaults (or *compile-file-truename* *load-truename*)))))
@@ -28,6 +30,25 @@
                    (return samples))
                  (dotimes (i length) (read-byte stream))))))
 
+(defun diff (a b)
+  (if (/= (length a) (length b))
+      (list :length :expected (length a) :got (length b))
+      (loop for i from 0 below (length a)
+            do (when (/= (aref a i) (aref b i))
+                 (return (list :mismatch :at i :expected (aref a i) :got (aref b i)))))))
+
+(defun sample-files (file)
+  (values (make-pathname :type "wav" :defaults file)
+          (make-pathname :type "qoa" :defaults file)
+          (make-pathname :type "wav" :name (format NIL "~a.qoa" (pathname-name file)) :defaults file)))
+
+(defun generate-reference (file)
+  (multiple-value-bind (source encoded decoded) (sample-files file)
+    (uiop:run-program (list *qoaconv* (uiop:native-namestring source) (uiop:native-namestring encoded))
+                      :output T :error-output T)
+    (uiop:run-program (list *qoaconv* (uiop:native-namestring encoded) (uiop:native-namestring decoded))
+                      :output T :error-output T)))
+
 (define-test interface
   :parent qoa
   (finish (qoa:encode-from-buffer (silence)))
@@ -40,15 +61,20 @@
   :depends-on (interface))
 
 (defmacro define-sample-test (name file)
-  `(define-test ,name
-     :parent samples
-     (let ((source (wav-samples ,(make-pathname :type "wav" :defaults file)))
-           (decoded (wav-samples ,(make-pathname :type "wav" :name (format NIL "~a.qoa" (pathname-name file)) :defaults file)))
-           (encoded (alexandria:read-file-into-byte-vector ,file)))
-       (is equalp encoded (qoa:encode-file source 'vector))
-       (is equalp decoded (qoa:decode-file encoded)))))
+  (multiple-value-bind (source encoded decoded) (sample-files file)
+    `(define-test ,name
+       :parent samples
+       (let ((source (wav-samples ,source))
+             (encoded (alexandria:read-file-into-byte-vector ,encoded))
+             (decoded (wav-samples ,decoded)))
+         (is eql NIL (diff encoded (qoa:encode-file source 'vector)))
+         (is eql NIL (diff decoded (qoa:decode-file encoded)))))))
 
 (macrolet ((define-all-samples ()
              `(progn ,@(loop for file in (directory (make-pathname :name :wild :type "qoa" :defaults *samples-directory*))
                              collect `(define-sample-test ,(intern (string-upcase (pathname-name file))) ,file)))))
   (define-all-samples))
+
+
+(DEFINE-SAMPLE-TEST SILENCE
+  #P"/home/linus/Projects/cl/cl-qoa/samples/silence.qoa")
